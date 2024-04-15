a6b5047c533ff293e8fbc286384699e2cdff0e99
Kris Fragata <kristinnefragata@outlook.com>
2024-04-14 23:19:13 -0400

    now extracting forecast and publish date

--- scraper.go
+++ scraper.go
@@ -51,12 +51,60 @@ func extractData(htmlContent string) {
 		log.Fatal(err)
 	}
 
+	publishExpire := getPublishExpire(doc)
+	currentDateArray, _ := extractCurrentDate(publishExpire)
 	recentPosting := getRecentPosting(doc)
 	cfs, _ := extractCFS(recentPosting)
 	timePosted, _ := extractTimePosted(recentPosting)
 
-	fmt.Printf("%v CFS @ %v", cfs, timePosted)
+	forecastArray := extractForecast(doc)
 
+
+	fmt.Printf("%v CFS @ %v\n", cfs, timePosted)
+	fmt.Println(forecastArray)
+	fmt.Println("forecast length:", len(forecastArray))
+	fmt.Println("current date and expiry:", publishExpire)
+	fmt.Println("published:", currentDateArray)
+
+}
+
+func getPublishExpire(doc *goquery.Document) (string){
+ found := false
+ var desiredLine string
+ doc.Find("div").Each(func(i int, e *goquery.Selection) {
+		text := e.Text()
+		if strings.Contains(text, "Published:") {
+			found = true
+			lines := strings.Split(text, "\n")
+			for _, line := range lines {
+				if strings.Contains(line, "Published:") && strings.Contains(line, "Expires:") {
+					desiredLine = strings.TrimSpace(line)
+					break
+				}
+			}			
+		}
+	})
+
+ if !found {
+	log.Fatal("current date not found")
+ }
+ return desiredLine
+}
+
+func extractCurrentDate(publishExpire string) ([]string, error) {
+	parts := strings.Fields(publishExpire)
+	var dateArray []string
+	if len(parts) >= 8 {
+		for i, v := range parts {
+			if (i < 0 &&  i < 7 ) {
+				dateArray = append(dateArray, v)
+			}
+		}
+	} else {
+		fmt.Println("Unable to extract publish date.")
+		return  []string{""}, errors.New("unable to extract publish date") 
+	}
+	return dateArray, nil
 }
 
 func getRecentPosting(doc *goquery.Document) string {
@@ -81,7 +129,7 @@ func extractCFS(recentPosting string) (string, error) {
 		cfs := parts[11]
 		return cfs, nil
 	} else {
-		fmt.Println("Unable to extract time and CFS measurement.")
+		fmt.Println("Unable to extract CFS measurement.")
 		return "", errors.New("unable to extract CFS") 
 	}
 }
@@ -92,23 +140,23 @@ func extractTimePosted(recentPosting string) (string, error){
 		time := parts[1] + " " + parts[2] + " " + parts[3]
 		return time, nil
 	} else {
-		fmt.Println("Unable to extract time and CFS measurement.")
+		fmt.Println("Unable to extract time.")
 		return "", errors.New("unable to extract time posted")
 	}
 }
 
-func extractForecast(htmlContent string) (string, error) {
-	found := false
-	var forecast string
-	doc.Find("div").Each(func(i int, e *goquery.Selection) {
-		if strings.Contains(e.Text(), "the total flow below the dam was") {
-			found = true
-			forecast = e.Text()
-			return
-		}
+func extractForecast(doc *goquery.Document) ([]string) {
+	var forecastArray []string
+	var forecastHtml string
+	doc.Find("div:contains('The following forecast for flows') + div").Each(func(i int, e *goquery.Selection) {
+			htmlContent, err := e.Html()
+			if err != nil {
+				log.Fatal("forecast not found")
+				return
+			}
+			forecastHtml = htmlContent
 	})
-	if !found {
-		log.Fatal("recent posting not found")
-	}
-	return forecast
+
+	forecastArray = strings.Split(forecastHtml, "<br/>")
+	return forecastArray
 }
