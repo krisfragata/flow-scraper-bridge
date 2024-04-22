f9513935666a2ca39b4d67e35b7310ac07fdf0af
Kris Fragata <kristinnefragata@outlook.com>
2024-04-22 16:16:10 -0400

    working on  optimizing queries

--- db.go
+++ db.go
@@ -11,7 +11,9 @@ import (
 	"github.com/joho/godotenv"
 )
 
-
+type Release struct{
+	release_date string
+}
 
 type Data struct{
 	date_posted time.Time `json:"date_posted"`
@@ -22,25 +24,12 @@ type Data struct{
 	expires   string `json:"expires"`
 }
 
-type Schedule struct{
-	id int64
-	release_date string
-	last_flow string
-	minimum_release bool
-}
-
 func runDB(date time.Time, currentDate string, cfs string, timePosted string, forecast []string, expire string) {
-	// err := connectDB()
-	// if err != nil {
-	// 	log.Fatal("Error connecting to database", err)
-	// }
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
 	supabaseUrl := os.Getenv("SUPABASE_URL")
-	// supabaseKey := os.Getenv("SUPABASE_KEY")
-	// supabase := supa.CreateClient(supabaseUrl, supabaseKey)
 
 	conn, err := pgx.Connect(context.Background(), supabaseUrl)
 	if err != nil {
@@ -48,49 +37,39 @@ func runDB(date time.Time, currentDate string, cfs string, timePosted string, fo
 	}
 	defer conn.Close(context.Background())
 
-	rows, err := conn.Query(context.Background(), "SELECT * FROM scheduled_release")
+	rows, err := conn.Query(context.Background(), "SELECT release_date FROM scheduled_release") 
 	if err != nil {
 		log.Fatal("Error querying database:", err)
 	}
 	defer rows.Close()
 
-	var schedules []Schedule
+	var releases []Release
 	for rows.Next() {
-    var s Schedule
-    err := rows.Scan(s.id ,&s.release_date, &s.last_flow, s.minimum_release)
+		var r Release
+		err := rows.Scan(&r.release_date)
     if err != nil {
         log.Fatal(err)
     }
-   schedules = append(schedules, s)
-}
-if err := rows.Err(); err != nil {
+   releases = append(releases, r)
+	}
+	if err := rows.Err(); err != nil {
     log.Fatal(err)
-}
-
-	//insert data into supabase
-	// row := Data{
-	// 	date_posted: date,
-	// 	date_string: currentDate,
-	// 	current_cfs: cfs,
-	// 	time_posted: timePosted,
-	// 	forecast:    forecast,
-	// 	expires:     expire,
-	// }
-
-	// var results []Data
-	// err := supabase.DB.From("daily_data").Insert(row).Execute(&results)
-	// if err != nil{
-	// 	panic(err)
-	// }
+	}
 	
-	// var results map[string]interface{}
-	// err = supabase.DB.From("scheduled_release").Select("*").Execute(&results)
-	// if err != nil {
-	// 	panic(err)
-	// }
+
+	fmt.Println(releases)
+
+	// insert data into supabase
+	row := Data{
+		date_posted: date,
+		date_string: currentDate,
+		current_cfs: cfs,
+		time_posted: timePosted,
+		forecast:    forecast,
+		expires:     expire,
+	}
 
 
-	fmt.Println("data from scheduled release", schedules, "END")
+	fmt.Println("data from scheduled release", row, "END")
 	// fmt.Println(results)
-
 }
